964fd3c088b76abf4bc32025d3e9bc6351b3a742
Akashsuga <akashsuga1030@gmail.com>
2025-06-26 11:34:37 +0530

    Update lstm_app.py

--- lstm_app.py
+++ lstm_app.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1Fl8Gdi0FU_6GcYcq51ZLqmfNM4kN24zJ
 """
 
-!pip install matplotlib streamlit yfinance scikit-learn
 import streamlit as st
 import numpy as np
 import pandas as pd
